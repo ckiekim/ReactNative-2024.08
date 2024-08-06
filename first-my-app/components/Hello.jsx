0d217844e1fc60fa61c960ba4c3c760cfacafe8a
Cheolkie Kim <ckiekim@naver.com>
2024-08-06 10:35:31 +0900

    first-my-app

---
+++ first-my-app/components/Hello.jsx
@@ -0,0 +1,8 @@
+import { ThemedText } from '@/components/ThemedText';
+
+export default function Hello() {
+  return (<>
+    <ThemedText>이 부분을 수정했습니다.</ThemedText>
+    <ThemedText>컴포넌트를 새로 만들었습니다.</ThemedText>
+  </>);
+}
